5e9c275aa4ece56f3242648113c062118739be55
ilyosbekdeveloper <ilyosbekumirzoqov6@gmail.com>
2023-01-15 20:13:10 +0500

    finish

--- 3-modul/24-dars/3-topshiriq/script.js
+++ 3-modul/24-dars/3-topshiriq/script.js
@@ -1,11 +1,54 @@
 var arr = [10, 11, 12,  13, 14, 15];
 var p = [];
+// var q = []
 
 for (var i = 0; i < arr.length; i++){
     if(arr[i]%2==0){
         p.push(arr[i])
-    }
+    } //else {
+    //     q.push(arr[i])
+    // }
 }
 p = p.join(' ')
+// q = q.join(' ')
 console.log(p + ' sonlari 2 ga bo\'linadi');
+// console.log(q + ' sonlari 2 ga bo\'linmaydi');
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function foo (a, b, v) {
+//     return a + b + v;
+// }
+// // console.log(foo(5, 3, 2));
+// var boo = foo(12, 23, 12);
+// console.log(boo)
+
+// function rec (a){
+//     console.log(a);
+//     a++;
+//     if(a > 15){
+//         return;
+//     }
+//     rec(a)
+// }
